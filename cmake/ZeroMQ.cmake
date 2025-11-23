0f5731be8d4e75b18a48c41583643d13ed20223d
Tomonori Takahashi <tomonori@rcnp.osaka-u.ac.jp>
2025-11-23 02:22:46 +0000

    rename cmake/ZeorMQ.cmake -> cmake/ZeroMQ.cmake

--- cmake/ZeroMQ.cmake
+++ cmake/ZeroMQ.cmake
@@ -5,7 +5,7 @@
 # =========================================================
 # 1. Try to find an installed Boost 
 # =========================================================
-find_package(ZeroMQ Config QUIET)
+find_package(ZeroMQ CONFIG QUIET)
 
 if(ZeroMQ_FOUND)
   message(STATUS "Found system ZeroMQ")
@@ -17,20 +17,31 @@ else()
   # 2. Use ZeroMQ from a git submodule (if available)
   # =========================================================
   set(ZEROMQ_SUBMODULE_PATH "${CMAKE_SOURCE_DIR}/external/libzmq")
-  if(EXITS "${ZEROMQ_SUBMODULE_PATH}")
+  if(EXISTS "${ZEROMQ_SUBMODULE_PATH}")
     message(STATUS "Using ZeroMQ submodule at ${ZEROMQ_SUBMODULE_PATH}")
 
     add_subdirectory(${ZEROMQ_SUBMODULE_PATH} EXCLUDEFOM_ALL)
 
   else()
-    message(STATU "No ZeroMQ submodule --- fetching from GitHub")
+    message(STATUS "No ZeroMQ submodule --- fetching from GitHub")
     # =========================================================
     # 3. Use FetchContent to fetch and build ZeroMQ from GitHub
     # =========================================================
     include(FetchContent)
     FetchContent_Declare(
-      zeromq https://github.com/zeromq/libzmq.git 
+      zeromq 
+      GIT_REPOSITORY https://github.com/zeromq/libzmq.git 
       GIT_TAG ${ZEROMQ_GIT_TAG}
+      CMAKE_ARGS
+        -DCMAKE_BUILD_TYPE=Release
+        -DCMAKE_INSTALL_PREFIX=${CMAKE_INSTALL_PREFIX}
+        -DCMAKE_CXX_STANDARD=${CMAKE_CXX_STANDARD}
+        -DCMAKE_EXE_LINKER_FLAGS=${CMAKE_EXE_LINKER_FLAGS}
+        -DCMAKE_SHARED_LINKER_FLAGS=${CMAKE_SHARED_LINKER_FLAGS}
+        -DCAKE_POSITION_INDEPENDENT_CODE=${CMAKE_POSITIN_INDEPENDENT_CODE}
+        -DCMAKE_SKIP_BUILD_RPATH=${CMAKE_SKIP_BUILD_RPATH}
+        -DCMAKE_INSTALL_RPATH_USE_LINK_PATH=${CMAKE_INSTALL_RPATH_USE_LINK_PATH}
+        -DCMAKE_BUILD_WITH_INSTALL_RPATH=${CMAKE_BUILD_WITH_INSTALL_RPATH}
     )
     FetchContent_MakeAvailable(zeromq)
   endif()
